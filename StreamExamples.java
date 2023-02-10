377035fd67c124869c0a219217afa96cd952b166
kaushikbhat07 <kaushik.bhat11@learner.manipal.edu>
2023-02-10 13:26:49 +0530

    Stream Operations

--- StreamExamples.java
+++ StreamExamples.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -49,6 +51,36 @@ public class StreamExamples {
         // figure out
 
         // 4. Stream Operations
+        List<String> memberNames = new ArrayList<>();
+        memberNames.add("Amitabh");
+        memberNames.add("Shekhar");
+        memberNames.add("Aman");
+        memberNames.add("Rahul");
+        memberNames.add("Shahrukh");
+        memberNames.add("Salman");
+        memberNames.add("Yana");
+        memberNames.add("Lokesh");
 
+        // 4.1. Intermediate Operations
+        // They return the stream itself, so you can chain multiple methods calls in a row.
+        memberNames.stream().filter(str -> str.startsWith("Am")).forEach(System.out::println);
+
+        // The map() intermediate operation converts each element in the stream into another object via the given function.
+        memberNames.stream().map(str -> new StringBuffer(str))
+                .forEach(s -> System.out.println(s));
+
+        // 4.2. Terminal operations
+        // They return a result of a certain type after processing all the stream elements.
+
+        Optional<String> reducedString = memberNames.stream().reduce((s1, s2) -> s1 + s2);
+
+        // 5. Short-circuit Operations
+        // Though stream operations are performed on all elements inside a collection satisfying a Predicate,
+        // it is often desired to break the operation whenever a matching element is encountered during iteration.
+
+        boolean matched = memberNames.stream().anyMatch((s) -> s.startsWith("A"));
+
+        // 6. Parallel Streams
+        boolean matchYes = memberNames.parallelStream().anyMatch((s) -> s.startsWith("A"));
     }
 }
